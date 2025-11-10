00396b5e578f41d6d264eaadbc64ac159e74bdeb
zukolime <alena.perminova.1993@gmail.com>
2025-11-10 11:40:38 +0500

    fix: configurations for deploy on GitPages

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,4 +10,5 @@ export default defineConfig({
       },
     }),
   ],
-})
+  base: '/app/',
+});
